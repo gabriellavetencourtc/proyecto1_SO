3c50bf1fa99b04122b70a77935a3cbbe031367a0
gabriellavetencourtc <103976921+gabriellavetencourtc@users.noreply.github.com>
2024-02-02 11:25:38 -0400

    comienzo

---
+++ src/p1_so/Trabajadores.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p1_so;
+
+import static java.lang.Thread.sleep;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author gabriellavetencourt
+ */
+public class Trabajadores extends Thread {
+    //19 empleados
+    //guinostas y disenador 1  c 4 dias
+    //animador 1 al dia
+    //actor 5 doblajes al dia 
+    private String nombre;
+    private double tipo;
+    private double sueldo;
+    private double sueldoAcumulado;
+    private String empresa;
+    private Drive drive;
+    private int duracionDia;
+    private double produccionDiaria;
+    private double diasParaCompletar; //dias que se tarda un trabajador en completar su tarea
+    private Semaphore mutex;
+
+    public Trabajadores(String nombre, double tipo, String empresa, Drive drive, int duracionDia, float produccionDiaria, float diasParaCompletar, Semaphore mutex) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.sueldo = sueldo();
+        this.sueldoAcumulado = 0;
+        this.empresa = empresa; //nombre de la empresa
+        this.drive = drive;
+        this.duracionDia = duracionDia;
+        this.produccionDiaria = produccionDiaria();
+        this.diasParaCompletar = 0;
+        this.mutex = mutex;
+    }
+    
+    @Override
+    public void run(){
+         while(true) {           
+            try {
+                sueldo();
+                produccionDiaria();
+                System.out.println("Trabajador: "+ this.nombre + " gana: "+ salarioTotal() +"$");
+                sleep(this.duracionDia);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Trabajadores.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public double sueldo(){
+        if (getTipo() == 0) { // guionista
+            return 20;
+        }
+        if(getTipo() == 1){ //diseñador
+            return 26;
+        } 
+        if (getTipo() == 2){ //animador
+            return 40;
+        }
+        if(getTipo() ==3){ //doblaje
+            return 16;
+        }
+        if(getTipo() ==4){ //guionista plotTwist
+            return 34;    
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    public double produccionDiaria(){
+        if (getTipo() == 0) { // guionista
+            return 0.26;
+        }
+        if(getTipo() == 1){ //diseñador
+            return 0.26;
+        } 
+        if (getTipo() == 2){ //animador
+            return 1.1;
+        }
+        if(getTipo() ==3){ //doblaje
+            return 5;
+        }
+        if(getTipo() ==4){ //guionista plotTwist
+            return 0.51;    
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    public double salarioTotal(){
+        double sueldo = getSueldoAcumulado() + getSueldo()*24;
+        setSueldoAcumulado(sueldo);
+        return sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(double tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public double getSueldoAcumulado() {
+        return sueldoAcumulado;
+    }
+
+    public void setSueldoAcumulado(double sueldoAcumulado) {
+        this.sueldoAcumulado = sueldoAcumulado;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public Drive getDrive() {
+        return drive;
+    }
+
+    public void setDrive(Drive drive) {
+        this.drive = drive;
+    }
+
+    public int getDuracionDia() {
+        return duracionDia;
+    }
+
+    public void setDuracionDia(int duracionDia) {
+        this.duracionDia = duracionDia;
+    }
+
+    public double getProduccionDiaria() {
+        return produccionDiaria;
+    }
+
+    public void setProduccionDiaria(double produccionDiaria) {
+        this.produccionDiaria = produccionDiaria;
+    }
+
+    public double getDiasParaCompletar() {
+        return diasParaCompletar;
+    }
+
+    public void setDiasParaCompletar(double diasParaCompletar) {
+        this.diasParaCompletar = diasParaCompletar;
+    }
+
+    public Semaphore getMutex() {
+        return mutex;
+    }
+
+    public void setMutex(Semaphore mutex) {
+        this.mutex = mutex;
+    }
+    
+
+
+    
+}
