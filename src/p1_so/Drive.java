d288f28064c4f53f625ce1eddff609266b536fb8
gabriellavetencourtc <103976921+gabriellavetencourtc@users.noreply.github.com>
2024-02-07 09:44:09 -0400

    drive

--- src/p1_so/Drive.java
+++ src/p1_so/Drive.java
@@ -19,12 +19,13 @@ public class Drive {
     private int deadline;
     private int capitulo;
     private int capituloPlotTwist;
+    private int contador;
     private JLabel[] labels;
-    private int empresa;
+    private String empresa;
     private double salario;
     private int diasTranscurridos;
 
-    public Drive(int empresa) {
+    public Drive(String empresa) {
         this.guion = 0;
         this.escenario = 0;
         this.animacion = 0;
@@ -34,11 +35,180 @@ public class Drive {
         this.deadline = 0;
         this.capitulo = 0;
         this.capituloPlotTwist = 0;
+        this.contador = 0;
         this.empresa = empresa;
         this.salario = 0;
         this.diasTranscurridos = 0;
     }
+    
+    public void aggDrive(int cantidad, int tipo) {
 
+        if (tipo == 0) { // guionista
+            if (this.guion < 25) {
+                if ((this.guion + cantidad) > 25) {
+                    this.guion += (25 - this.guion);
+
+                } else {
+                    this.guion += cantidad;
+                }
+
+                this.labels[0].setText(Integer.toString(this.guion));
+
+            }
+        }
+        if (tipo == 1) { //diseñador
+            if (this.escenario < 20) {
+                if ((this.escenario + cantidad) > 25) {
+                    this.escenario += (25 - this.escenario);
+
+                } else {
+                    this.escenario += cantidad;
+                }
+
+                this.labels[1].setText(Integer.toString(this.escenario));
+
+            }
+            if (tipo == 2) { //animador
+                if (this.animacion < 55) {
+                    if ((this.animacion + cantidad) > 55) {
+                        this.animacion += (55 - this.animacion);
+
+                    } else {
+                        this.animacion += cantidad;
+                    }
+
+                    this.labels[2].setText(Integer.toString(this.animacion));
+
+                }
+
+            }
+            if (tipo == 3) { //doblaje
+                if (this.doblaje < 35) {
+                    if ((this.doblaje + cantidad) > 35) {
+                        this.doblaje += (35 - this.doblaje);
+
+                    } else {
+                        this.doblaje += cantidad;
+                    }
+
+                    this.labels[3].setText(Integer.toString(this.doblaje));
+
+                }
+
+            }
+            if (tipo == 4) { //guionista plotTwist
+                if (this.plotwist < 10) {
+                    if ((this.plotwist + cantidad) > 10) {
+                        this.plotwist += (10 - this.plotwist);
+
+                    } else {
+                        this.plotwist += cantidad;
+                    }
+
+                    this.labels[4].setText(Integer.toString(this.plotwist));
+
+                }
+            }
+        }
+    }
+    
+        public boolean ReqCapitulo() {
+        if(this.empresa.equalsIgnoreCase("Nickelodeon")){
+            if(this.contador >= 5){
+                return this.guion >= 2 && this.escenario >= 1 && this.animacion >= 4 && this.doblaje >= 4 && this.plotwist >= 2;
+                
+            }else{
+                return this.guion >= 2 && this.escenario >= 1 && this.animacion >= 4 && this.doblaje >= 4;
+            }
+        }
+            
+        if(this.empresa.equalsIgnoreCase("Cartoon Network")){
+            if(this.contador >= 3){
+                return this.guion >= 1 && this.escenario >= 2 && this.animacion >= 6 && this.doblaje >= 5 && this.plotwist >= 1;
+                
+            }else{
+                return this.guion >= 1 && this.escenario >= 2 && this.animacion >= 6 && this.doblaje >= 5;
+            }
+            
+        }
+        
+         if(this.empresa.equalsIgnoreCase("Star Channel")){
+            if(this.contador >= 6){
+                return this.guion >= 2 && this.escenario >= 3 && this.animacion >= 4 && this.doblaje >= 6 && this.plotwist >= 5;
+                
+            }else{
+                return this.guion >= 2 && this.escenario >= 3 && this.animacion >= 4 && this.doblaje >= 6;
+            }
+        
+        }
+         return false; //pilas
+        }
+        
+            public void crearCapitulo() {
+        if(this.empresa.equalsIgnoreCase("Nickelodeon")){
+            this.guion -= 2;
+            this.escenario -= 1;
+            this.animacion -= 4;
+            this.doblaje -= 4;
+            
+            if(this.contador >= 5){
+                this.plotwist -= 2;
+                this.contador = 0;
+                this.capituloPlotTwist += 1;
+                
+            }else{
+                this.contador += 1;
+                this.capitulo += 1;
+            }
+                
+        }
+        
+        if(this.empresa.equalsIgnoreCase("Cartoon Network")){
+            this.guion -= 1;
+            this.escenario -= 2;
+            this.animacion -= 6;
+            this.doblaje -= 5;
+            
+            if(this.contador >= 3){
+                this.plotwist -= 1;
+                this.contador = 0;
+                this.capituloPlotTwist += 1;
+                
+            }else{
+                this.contador += 1;
+                this.capitulo += 1;
+            }
+                
+        }
+        
+        if(this.empresa.equalsIgnoreCase("Star Channel")){
+            this.guion -= 2;
+            this.escenario -= 3;
+            this.animacion -= 4;
+            this.doblaje -= 6;
+            
+            if(this.contador >= 6){
+                this.plotwist -= 5;
+                this.contador = 0;
+                this.capituloPlotTwist += 1;
+                
+            }else{
+                this.contador += 1;
+                this.capitulo += 1;
+            }
+                
+        }     
+        this.labels[0].setText(Integer.toString(this.guion));
+        this.labels[1].setText(Integer.toString(this.escenario));
+        this.labels[2].setText(Integer.toString(this.animacion));
+        this.labels[3].setText(Integer.toString(this.doblaje));
+        this.labels[4].setText(Integer.toString(this.plotwist));
+        this.labels[6].setText(Integer.toString(this.capitulo));
+        this.labels[7].setText(Integer.toString(this.capituloPlotTwist));
+         
+    }
+    
+    
     public int getGuion() {
         return guion;
     }
@@ -85,7 +255,9 @@ public class Drive {
 
     public void setDiasRestantes(int diasRestantes) {
         this.diasRestantes = diasRestantes;
+        this.labels[5].setText(Integer.toString(this.diasRestantes));
     }
+   
 
     public int getDeadline() {
         return deadline;
@@ -119,11 +291,11 @@ public class Drive {
         this.labels = labels;
     }
 
-    public int getEmpresa() {
+    public String getEmpresa() {
         return empresa;
     }
 
-    public void setEmpresa(int empresa) {
+    public void setEmpresa(String empresa) {
         this.empresa = empresa;
     }
 
